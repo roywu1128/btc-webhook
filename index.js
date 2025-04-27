e96d85a0ab777f4a8c49ac80bd04c0d704ff89d8
roywu1128 <ksstp526@gmail.com>
2025-04-27 10:07:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  console.log('收到Webhook訊息:', req.body);
+
+  res.status(200).json({ message: 'Webhook Received Successfully' });
+}
