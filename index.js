64ad1455efe23e31eb7ac4271b841b5345b0850e
roywu1128 <ksstp526@gmail.com>
2025-04-27 10:33:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ export default async function handler(req, res) {
 
   console.log('收到Webhook訊息:', req.body);
 
-  res.status(200).json({ message: 'Webhook Received Successfully' });
+  res.status(200).json({
+    message: 'Webhook Received Successfully',
+    data: req.body,  // 把收到的資料也一起回傳
+  });
 }
